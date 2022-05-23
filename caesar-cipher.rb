4b03450721952a2edbc5eab57c50f6b3ec086f2e
Tygh Walters <tyghwalters@gmail.com>
2022-05-23 10:42:31 -0700

    Remove minor white space. Testing a push after relocation the directory on my local machine.

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,8 +1,8 @@
-# User string + number instructions: 
-puts "Welcome to Tygh's Caesar Cipher! 
+# User string + number instructions:
+puts "Welcome to Tygh's Caesar Cipher!
 Enter a message you wish to encrypt then hit return."
 userstringInput = gets.chomp
-puts "Next, choose your cipher key. 
+puts "Next, choose your cipher key.
 Enter number between 1-26, then hit return to finish."
 usernumInput = gets.chomp.to_i #convert a string (from `gets``) to integer
 # If user enters a number outside the allowed range,
@@ -23,22 +23,21 @@ def caesar_cipher (string, shiftFactor)
   arrayShifted = arrayOrdinal.map do |ordNum| # remember, "Each always returns the original, unchanged object."
     if ordNum.between?(97, 122) # lowercase
       adjusted_index = (ordNum + shiftFactor) % 97
-      shiftedOrdNum = (97..122).to_a[adjusted_index - 26] 
+      shiftedOrdNum = (97..122).to_a[adjusted_index - 26]
       shiftedOrdNum
     elsif ordNum.between?(65, 90) # uppercase
       adjusted_index = (ordNum + shiftFactor) % 65
-      shiftedOrdNum = (65..90).to_a[adjusted_index - 26] 
+      shiftedOrdNum = (65..90).to_a[adjusted_index - 26]
       shiftedOrdNum
     else #no shift (keep num)
       ordNum # w/o this it added nils for non-letters
     end
   end
   # Find the corresponding letter of the ordNum (wrap from z to a.)
-  shiftedArrayCharacterized = arrayShifted.map { |num| num.chr } 
+  shiftedArrayCharacterized = arrayShifted.map { |num| num.chr }
   # FINALLY, output the array into a string (keep case)
   sleep(2.0) # pauses in sec before showing below for UX...
-  puts messageEncrypted = 
-    "Your encrypted message is: \"#{shiftedArrayCharacterized.join("")}\""  
-end 
+  puts messageEncrypted = "Your encrypted message is: \"#{shiftedArrayCharacterized.join("")}\""  
+end
 
 caesar_cipher(userstringInput, usernumInput)
